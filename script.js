11aac2ab1fb9ac1776f4307f11f1a29a0f2b0b65
Onuh Kingsley <onukingsleyonu@gmail.com>
2019-11-13 11:31:25 +0100

    updated file "<h4> Current weather:</h4>"  + "<h5>  </h5>" + data.weather[0].main;

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 	$('#btn').click(function(){
-		let location = $('#search').val();
-		if(location == ''){
-			$('#error').html('Please enter a valid loaction');
-		}else{
-			$.ajax({
-				url: 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric' + '&APPID=1a3d68088228d76d61fa392c2a9ade0a',	
+			let location = $('#search').val();
+			if (location != '') {
+				$.ajax({
+					url: 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric' + '&APPID=1a3d68088228d76d61fa392c2a9ade0a',	
 
 				type: "GET",
 				datatype: JSON,
@@ -20,13 +18,17 @@ $(document).ready(function(){
 					let display = displayData(data);
 					$('#display').html(display);
 					$('#search').val('');
-					
 				}
-			});
-		}
+				});
+			}else{
+				$('#error').html('Please enter a valid loaction name');
+			}
 	});
+
 });
 function displayData(data){
-return "<h4> Current weather:</h4>"  + "<h5>  </h5>" + data.weather[0].main;
+	return
+	"<h4> Current weather:</h4>"  + "<h5>  </h5>" + data.weather[0].main;
+	
 
 }
