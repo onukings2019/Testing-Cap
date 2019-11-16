304f1e7f5a2d3036dfe2f9f0da829ae0cabc94fc
Onuh Kingsley <onukingsleyonu@gmail.com>
2019-11-16 11:56:27 +0100

    Update

--- script.js
+++ script.js
@@ -17,9 +17,15 @@ $(document).ready(function(){
 				type: "GET",
 				datatype: JSON,
 				success: function(data){
-					console.log(data);
+					let display = displayData(data);
+					$('#display').html(display);
+					$('#search').val('');
 				}
 			});
 		}
 	});
 });
+function displayData(data){
+return "<h4> Current weather for:</h4>"  + location + "<h5>  </h5>" + data.weather[0].main;
+
+}
